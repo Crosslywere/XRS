55cfcd78129138d701856d7e1192b1dcd9b17d49
Crosslywere <ogboruj@outlook.com>
2025-09-07 21:56:25 +0100

    Added glfw to test code

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,18 @@
 #include <xrs/Renderer.hpp>
+#include <GLFW/glfw3.h>
 
 int main(void)
 {
-  
+  glfwInit();
+  auto window = glfwCreateWindow(800, 600, "Testing Window", 0, 0);
+  glfwMakeContextCurrent(window);
+  auto renderer = xrs::Renderer::Initialize(true, (xrs::Renderer::GLLoaderFunc)glfwGetProcAddress);
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+  renderer.Shutdown();
+  glfwDestroyWindow(window);
+  glfwTerminate();
   return 0;
 }
