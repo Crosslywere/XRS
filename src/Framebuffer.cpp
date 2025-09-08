e48b4587a08b7ada8849ab46569c71c99c3aafb4
Crosslywere <ogboruj@outlook.com>
2025-09-08 01:08:59 +0100

    Fixed compilation error

--- src/Framebuffer.cpp
+++ src/Framebuffer.cpp
@@ -13,7 +13,8 @@ namespace xrs
   Framebuffer::Framebuffer(int width, int height, unsigned int colorBufferCount, const BufferConfigFlags bufferConfigFlags)
       : m_ColorBufferCount{colorBufferCount}, m_BufferConfigFlags{bufferConfigFlags},
       m_Width{width}, m_Height{height},
-      m_HasDepthBuffer{bufferConfigFlags & (BufferConfigFlags::DEPTH_RENDER_BUFFER | BufferConfigFlags::DEPTH_TEXTURE_BUFFER | BufferConfigFlags::DEPTH_TEXTURE_3D_BUFFER)}
+      m_HasDepthBuffer{static_cast<bool>(bufferConfigFlags & (BufferConfigFlags::DEPTH_RENDER_BUFFER |
+        BufferConfigFlags::DEPTH_TEXTURE_BUFFER | BufferConfigFlags::DEPTH_TEXTURE_3D_BUFFER))}
   {
     if (m_HasDepthBuffer)
       std::printf("Framebuffer created has depth buffer!");
