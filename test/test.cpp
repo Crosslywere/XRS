33f119b1663354eeb58b278b557577a1a1a06a21
Crosslywere <ogboruj@outlook.com>
2025-09-11 22:45:13 +0100

    Updated test code - Updated function references from renderer - Specified window hints - Created and called processInputs function

--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,29 @@
 #include <xrs/Renderer.hpp>
 #include <GLFW/glfw3.h>
 
+void processInputs(GLFWwindow* window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
 int main(void)
 {
   glfwInit();
-  auto window = glfwCreateWindow(800, 600, "Testing Window", 0, 0);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+  auto window = glfwCreateWindow(800, 600, "Testing Window", nullptr, nullptr);
   glfwMakeContextCurrent(window);
   auto renderer = xrs::Renderer::Initialize(true, (xrs::Renderer::GLLoaderFunc)glfwGetProcAddress);
   while (!glfwWindowShouldClose(window))
   {
-    renderer.Begin();
-    renderer.End();
+    renderer.BeginFrame();
+    renderer.EndFrame();
     glfwPollEvents();
     glfwSwapBuffers(window);
+    processInputs(window);
   }
   renderer.Shutdown();
   glfwDestroyWindow(window);
